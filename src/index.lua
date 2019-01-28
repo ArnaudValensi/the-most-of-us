9a4afcf35ded2267d4433f41454dbe99a9d3da55
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-01-28 02:35:00 +0100

    Change cat color

--- src/index.lua
+++ src/index.lua
@@ -2,7 +2,7 @@ gameobjects = require_game_objects()
 
 local player = gameobjects:new("player")
 player:add_component(new_transform_comp(1 * 8, 0))
-player:add_component(new_sprite_comp({ sprite_number = 1 }))
+player:add_component(new_sprite_comp({ sprite_number = 64 }))
 player:add_component(new_player_comp(3))
 
 function _init()
