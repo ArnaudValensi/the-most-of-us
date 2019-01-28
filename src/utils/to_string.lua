b5868e596f4035f0e792d00d9a4ad4477d4518f8
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-01-28 13:36:54 +0100

    Add a to_string util

---
+++ src/utils/to_string.lua
@@ -0,0 +1,15 @@
+function to_string(any)
+  if type(any)=="function" then return "function" end
+  if any==nil then return "nil" end
+  if type(any)=="string" then return any end
+  if type(any)=="boolean" then return any and "true" or "false" end
+  if type(any)=="number" then return ""..any end
+  if type(any)=="table" then -- recursion
+    local str = "{ "
+    for k,v in pairs(any) do
+      str=str..tostring(k).."->"..tostring(v).." "
+    end
+    return str.."}"
+  end
+  return "unkown" -- should never show
+end
